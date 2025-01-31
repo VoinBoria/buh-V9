f023f3103c36f86dc10b48165263a7078a95f1fc
VoinBoria <voinboria@gmail.com>
2025-01-31 14:39:41 +0200

    Update BudgetPlanningActivity.kt

--- BudgetPlanningActivity.kt
+++ BudgetPlanningActivity.kt
@@ -11,7 +11,10 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -31,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -744,7 +748,6 @@ fun AddGoalDialog(
         containerColor = Color.DarkGray.copy(alpha = 0.8f) // Додана прозорість до самого меню
     )
 }
-
 // Додано: функція для форматування відсотка
 fun Double.format(digits: Int) = "%.${digits}f".format(this)
 
